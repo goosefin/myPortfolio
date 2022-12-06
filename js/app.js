4266ddbeaf7d2967a12152c6acc2495f203f2ad8
goosefin <agustinafin96@gmail.com>
2022-12-05 20:00:00 -0500

    started working on footer links

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('Here')
+let linkedin = document.getElementById('linkedin-img')
+let github = document.getElementById('github-img')
+let resume = document.getElementById('resume-img')
+
+// const openLinkedIn = () =>{
+    
+// }
+
+// const openGitHub = () =>{
+
+// }
+
+// const openResume = () =>{
+
+// }
+
+linkedin.addEventListener('click', () => {
+    window.open('www.linkedin.com/in/agustina-fin', '_blank')
+})
+
+github.addEventListener('click', () => {
+    alertIt()
+})
+
+resume.addEventListener('click', () => {
+    alertIt()
+})
