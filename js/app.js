974863c4eee39ec059041b8ade49edd33b776ce6
goosefin <agustinafin96@gmail.com>
2023-02-05 17:58:23 -0500

    added links to footer images

--- js/app.js
+++ js/app.js
@@ -15,13 +15,13 @@ let resume = document.getElementById('resume-img')
 // }
 
 linkedin.addEventListener('click', () => {
-    window.open('www.linkedin.com/in/agustina-fin', '_blank')
+    window.open('https://www.linkedin.com/in/agustina-fin', '_blank')
 })
 
 github.addEventListener('click', () => {
-    alertIt()
+    window.open('https://github.com/goosefin', '_blank')
 })
 
 resume.addEventListener('click', () => {
-    alertIt()
+    window.open('https://docs.google.com/document/d/1SIZu5EYgMRr1LMFZ5cy1YVdIq5tKPhVK_RA5FId7i8c/edit?usp=sharing','_blank')
 })
